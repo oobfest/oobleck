7d590db3393ee45e2fe90fb92b9a1b9160f6caa4
Tyler Lane <sld.potato@gmail.com>
2018-01-18 22:30:14 -0600

    More env stuff

--- index.js
+++ index.js
@@ -5,4 +5,20 @@ const PORT = process.env.PORT || 3000
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(PORT, () => console.log('Example app listening on port', PORT))
 
-console.log(process.env)
+// Database stuff
+var mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_CONNECTION)
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function() {
+	console.log("Connected!")
+
+	var kittySchema = mongoose.Schema({ name: String })
+	var Kitten = mongoose.model('Kitten', kittySchema)
+	Kitten.find(function (err, kittens) { console.log(kittens) })
+
+	var animalSchema = mongoose.Schema({ type: String })
+	var Animal = mongoose.model('Animal', animalSchema)
+	Animal.find(function (err, animals) { console.log(animals) })
+}
