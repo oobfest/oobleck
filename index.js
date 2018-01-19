221511944885c94414155f3d0e6d3055319beb5a
Tyler Lane <sld.potato@gmail.com>
2018-01-18 22:22:42 -0600

    Heroku environment variables

--- index.js
+++ index.js
@@ -2,21 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
-let dbConnected = false
-
 app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/test', (req, res) => res.send('Database connected: ' + dbConnected))
-
 app.listen(PORT, () => console.log('Example app listening on port', PORT))
 
-// Database stuff
-var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/test')
-
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'))
-db.once('open', function() {
-  // we're connected!
-  console.log("Connected!")
-  dbConnected = true
-})
+console.log(process.env.HEROKU)
