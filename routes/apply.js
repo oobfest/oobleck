31cccb932224822d6b08eb2543f73a4a307a5650
Tyler Lane <sld.potato@gmail.com>
2018-01-20 01:02:35 -0600

    Creating application form, added Charming Pro

--- routes/apply.js
+++ routes/apply.js
@@ -2,11 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (request, response) => {
-	response.render('apply')
-})
-
-router.post('/', (request, response)=> {
-	response.render('thank-you')
+	response.render('apply/apply')
 })
 
 module.exports = router
