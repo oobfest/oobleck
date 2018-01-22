9373ffc8c018be0c0746c36ef50c927657478700
Tyler Lane <sld.potato@gmail.com>
2018-01-22 15:35:54 -0600

    Refactoring, more submission features

--- app.js
+++ app.js
@@ -36,37 +36,6 @@ app.post('/upload', (request, response) => {
 	response.send("Meow")
 })
 
-var aws = require('aws-sdk')
-
-const S3_BUCKET = process.env.AWS_BUCKET_NAME
-aws.config.region = 'us-east-2'
-
-// Retrieve a signed request from the app with which the image can be PUT to S3
-app.get('/uploady', (request, response) => {
-	const s3 = new aws.S3()
-	const fileName = request.query['file-name']
-	const fileType = request.query['file-type']
-	const s3Parameters = {
-		Bucket: S3_BUCKET,
-		Key: fileName,
-		Expires: 60,
-		ContentType: fileType,
-		ACL: 'public-read'
-	}
-	s3.getSignedUrl('putObject', s3Parameters, (error, data) => {
-		if(error) {
-			console.log(error)
-			return res.end()
-		}
-		const returnData = {
-			signedRequest: data,
-			url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
-		}
-		response.write(JSON.stringify(returnData))
-		response.end()
-	})
-})
-
 // Setup Database
 const db = require('./db/setup')
 
--- routes/apply.js
+++ routes/apply.js
@@ -5,4 +5,41 @@ router.get('/', (request, response) => {
 	response.render('apply/apply')
 })
 
+// This kinda fucks shit up
+router.get('/:id/:guid', (request, response) => {
+	response.send(request.params)
+})
+
+var aws = require('aws-sdk')
+aws.config.region = 'us-east-2'
+const S3 = new aws.S3()
+const S3_BUCKET = process.env.AWS_BUCKET_NAME
+
+// Retrieve a signed request from the app with which the image can be PUT to S3
+app.get('/image-upload-request', (request, response) => {
+	const fileName = request.query['file-name']
+	const fileType = request.query['file-type']
+	const s3Parameters = {
+		Bucket: S3_BUCKET,
+		Key: fileName,
+		Expires: 60,
+		ContentType: fileType,
+		ACL: 'public-read'
+	}
+	S3.getSignedUrl('putObject', s3Parameters, (error, data) => {
+		if(error) {
+			console.log(error)
+			return res.end()
+		}
+		const returnData = {
+			signedRequest: data,
+			url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+		}
+		response.write(JSON.stringify(returnData))
+		response.end()
+	})
+})
+
+
+
 module.exports = router
