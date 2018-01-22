42b4532c00c946f7d7ec35a232e7fb4c82f00d3d
Tyler Lane <sld.potato@gmail.com>
2018-01-22 02:12:11 -0600

    AWS stuff

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
+// Get .env data if not Heroku
+if (process.env.HEROKU !== 1) {
+	require('dotenv').load()
+}
+
 // Setup Express
 const express = require('express')
 const app = express()
@@ -21,6 +26,46 @@ app.use('/', routes)
 app.use('/kittens', kittens)
 app.use('/vue', vue)
 app.use('/apply', apply)
+
+// Fleeting, temporary
+app.get('/upload', (request, response) => {
+	response.render('upload')
+})
+
+app.post('/upload', (request, response) => {
+	response.send("Meow")
+})
+
+var aws = require('aws-sdk')
+
+const S3_BUCKET = process.env.AWS_BUCKET_NAME
+aws.config.region = 'us-east-2'
+
+// Retrieve a signed request from the app with which the image can be PUT to S3
+app.get('/uploady', (request, response) => {
+	const s3 = new aws.S3()
+	const fileName = request.query['file-name']
+	const fileType = request.query['file-type']
+	const s3Parameters = {
+		Bucket: S3_BUCKET,
+		Key: fileName,
+		Expires: 60,
+		ContentType: fileType,
+		ACL: 'public-read'
+	}
+	s3.getSignedUrl('putObject', s3Parameters, (error, data) => {
+		if(error) {
+			console.log(error)
+			return res.end()
+		}
+		const returnData = {
+			signedRequest: data,
+			url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+		}
+		response.write(JSON.stringify(returnData))
+		response.end()
+	})
+})
 
 // Setup Database
 const db = require('./db/setup')
