3352dab1a49fd700c241bee53ff62516a0809f20
Tyler Lane <sld.potato@gmail.com>
2018-07-05 21:21:57 -0500

    Volunteering form

--- volunteers/schema.js
+++ volunteers/schema.js
@@ -2,7 +2,22 @@ const mongoose = require('mongoose')
 
 let volunteerSchema = mongoose.Schema({
 	name: String,
-	email: String
+	email: String,
+  phone: String,
+  type: String,   // show tech driver party workshop
+  hours: Number,
+  volunteeredBefore: Boolean,
+  referral: String,
+  attendingOrientation: Boolean,
+
+  // Driver
+  carType: String,
+  passenegers: Number,
+  licensePlateNumber: String,
+
+  // Tech
+  techExperience: String,
+  volunteeredTechBefore: Boolean,
 })
 
 let Volunteer = mongoose.model('Volunteer', volunteerSchema)
