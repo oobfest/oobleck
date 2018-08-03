0ad621e12cdf43603b25314727ab56d86815d95d
Tyler Lane <sld.potato@gmail.com>
2018-08-03 04:30:52 -0500

    Draft of volunteer schedule

--- volunteers/view.controller.js
+++ volunteers/view.controller.js
@@ -2,11 +2,12 @@ const model = require('./model')
 
 module.exports = {
 	viewAll: function(request, response, next) {
-		model.getAll((error, volunteers)=> {
-			if(error) next(error)
-			else response.render('volunteers/view-all', { volunteers })
-		})
+		response.render('volunteers/view-all')
 	},
+
+  schedule: function(request, response, next) {
+    response.render('volunteers/schedule')
+  },
 
   apply: function(request, response, next) {
     response.render('volunteers/apply')
--- volunteers/view.routes.js
+++ volunteers/view.routes.js
@@ -6,6 +6,7 @@ const isNotARobot = require('../middleware/is-not-a-robot')
 const controller = require('./view.controller')
 
 router.get('/', isLoggedIn, controller.viewAll)
+router.get('/schedule', isLoggedIn, controller.schedule)
 router.get('/apply', controller.apply)
 
 module.exports = router
