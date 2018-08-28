e6bd02ddfa95b6c3f09496b694b8071721118004
Tyler Lane <sld.potato@gmail.com>
2018-08-28 05:40:34 -0500

    Code for email reminder, improve show filtering

---
+++ email/email-templates/show-reminder.js
@@ -0,0 +1,16 @@
+module.exports = function (shows) {
+return `
+<p>Hello!</p>
+
+<p>You are currently reserved for the following Out of Bounds shows:</p>
+
+<ul>
+${shows.map((show) => `<li>${show}</li>`).join('\n')}
+</ul>
+
+<p>Pick up your ticket(s) at the venue's box office <i>at least ten minutes before the show</i>. If you are late, you are at risk of losing your spot. No one is allowed to enter the theater 15 minutes after the show begins.</p>
+
+<p>If you have a badge, a friendly reminder that you must show your badge at the box office to retrieve your ticket. Please pick up your badge at registration, happening at The Hideout Theater (617 Congress Avenue) every evening of the festival from 6:30pm-10pm.</p>
+
+<p>Thank you!</p>
+`}
--- index.js
+++ index.js
@@ -30,3 +30,54 @@ process.on('uncaughtException', (error)=> {
 app.listen(process.env.PORT, ()=> {
 	log.info("🐵  Listening on port " + process.env.PORT)
 })
+
+/*
+let showsModel = require('./shows/model')
+let _ = require('lodash')
+let formatTime = function(time) { 
+  time = String(time)
+  return time.slice(0, time.length-2) + ":" + time.slice(time.length-2) + "pm" 
+}
+let formatVenue = function(venue) {
+  switch(venue) {
+    case 'Hideout Down': return 'Hideout Downstairs'
+    case 'Hideout Up': return 'Hideout Upstairs'
+    case 'ColdTowne': return 'ColdTowne'
+    case 'Fallout': return 'Fallout'
+    case 'Velveeta': return 'Velveeta Room'
+    case 'Spider House': return 'Spider House Ballroom'
+    case 'Institution': return 'Institution'
+    default: return venue
+  }
+}
+
+showsModel.getAll((error, shows)=> {
+  let showTickets = []
+  for(let i=0; i<shows.length; i++) {
+    for(let j=0; j<shows[i].tickets.length; j++) {
+      let ticket = shows[i].tickets[j]
+      ticket.showId = shows[i]._id
+      ticket.day = shows[i].day
+      ticket.venue = shows[i].venue
+      ticket.time = shows[i].time
+      ticket.paid = ticket.payment 
+        ? Number(ticket.payment.transactions[0].amount.total)
+        : 0
+      showTickets.push(ticket)
+    }
+  }
+  let ticketsByEmail = _.groupBy(showTickets, 'email')
+  let emails = Object.keys(ticketsByEmail)
+  for(let email of emails) {
+    let show = ticketsByEmail[email]
+
+    console.log(email)
+    let tickets = []
+    for(let ticket of show) {
+      tickets.push(`${ticket.quantity > 1 ? '(' + ticket.quantity + ') ' : '' }${ticket.day}, ${formatTime(ticket.time)}, ${formatVenue(ticket.venue)}`)
+    }
+    console.log(tickets)
+
+  }
+})
+*/
